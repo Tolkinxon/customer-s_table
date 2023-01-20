1af0a3494a3f80980875e8f61935f9f8122a1154
Tolkinxon <krosstony639@gmail.com>
2023-01-20 23:07:54 +0500

    a bit fixes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 
 function App() {
   function createData(name, amount, protein, storage_period, id) {
-    return { name, amount, protein, storage_period, id}
+    return { name, amount, protein, storage_period, id }
   }
 
   const rows = [
@@ -15,7 +15,13 @@ function App() {
     createData('cherry', 500, 0.9, 4, 1),
     createData('banana', 100, 5.9, 3, 0),
     createData('potatoes', 300, 9.7, 3, 1),
-    
+    createData('carrot', 780, 2.5, 1, 0),
+    createData('pie', 240, 9.4, 5, 1),
+    createData('tea', 350, 2.7, 3, 0),
+    createData('rice', 180, 9.9, 65, 1),
+    createData('cabbage', 144, 9.6, 39, 0),
+    createData('onion', 670, 89, 35, 1),
+
   ]
 
   return (
@@ -31,16 +37,13 @@ function App() {
         </TableHead>
         <TableBody>
           {rows.map((row) => (
-          <TableRow key={row.name} id={row.id} >
-            <Box>{row.name}</Box>
-            <Box>{row.amount}</Box>
-            <Box>{row.protein}</Box>
-            <Box>{row.storage_period}</Box>
-          </TableRow>
-
+            <TableRow key={row.name} id={row.id}>
+              <Box>{row.name}</Box>
+              <Box>{row.amount}</Box>
+              <Box>{row.protein}</Box>
+              <Box>{row.storage_period}</Box>
+            </TableRow>
           ))}
-     
-
         </TableBody>
       </Table>
     </div>
